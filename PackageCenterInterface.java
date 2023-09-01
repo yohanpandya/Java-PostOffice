0fe2e14eb13e333d7d5e13c30156569de05ef81f
yohanpandya <114615213+yohanpandya@users.noreply.github.com>
2023-09-01 11:22:36 -0700

    Create PackageCenterInterface.java

---
+++ PackageCenterInterface.java
@@ -0,0 +1,9 @@
+public interface PackageCenterInterface {
+	// returns name of this
+	public String getName();
+
+	// returns the position within the matrix of this PackagecenterFD.
+	public int getMatrixPosition();
+
+	public void setMatrixPosition(int pos);
+}
